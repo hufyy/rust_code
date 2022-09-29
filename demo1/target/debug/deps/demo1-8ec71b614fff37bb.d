233e30dc0e352a82663fa67fdbd9f4a5e4397b4f
hufyy <15232454240@163.com>
2022-09-29 16:16:19 +0800

    test

---
+++ demo1/target/debug/deps/demo1-8ec71b614fff37bb.d
@@ -0,0 +1,5 @@
+/home/ubuntu/rust_code/demo1/target/debug/deps/demo1-8ec71b614fff37bb.rmeta: src/main.rs
+
+/home/ubuntu/rust_code/demo1/target/debug/deps/demo1-8ec71b614fff37bb.d: src/main.rs
+
+src/main.rs:
---
+++ demo1/target/debug/deps/demo1-98d997223d8b5ecd.d
@@ -0,0 +1,5 @@
+/home/ubuntu/rust_code/demo1/target/debug/deps/demo1-98d997223d8b5ecd.rmeta: src/main.rs
+
+/home/ubuntu/rust_code/demo1/target/debug/deps/demo1-98d997223d8b5ecd.d: src/main.rs
+
+src/main.rs:
